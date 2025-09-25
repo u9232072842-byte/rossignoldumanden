c74439c9ac7b85306ab47e339b0bdd9174186ffc
ousmane yattus <u9232072842@gmail.com>
2025-09-25 21:18:37 +0000

    IL FAUT INIT FIREBASE

---
+++ src/firebase/client-provider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useMemo, type PropsWithChildren } from 'react';
+
+import type { FirebaseApp } from 'firebase/app';
+import type { Auth } from 'firebase/auth';
+import type { Firestore } from 'firebase/firestore';
+
+import { initializeFirebase, FirebaseProvider } from '@/firebase';
+
+export function FirebaseClientProvider({ children }: PropsWithChildren) {
+  const { firebaseApp, auth, firestore } = useMemo(initializeFirebase, []);
+
+  return <FirebaseProvider>{children}</FirebaseProvider>;
+}
