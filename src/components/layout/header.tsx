bf99ed3279755c778df55251768a151093129567
ousmane yattus <u9232072842@gmail.com>
2025-10-20 20:28:54 +0000

    retire la position fixe au header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -82,7 +82,7 @@ export default function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/50 bg-background/80 backdrop-blur-lg">
+    <header className="w-full border-b border-border/50 bg-background">
       <div className="container flex h-20 items-center">
         <div className="flex items-center md:hidden">
           <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
