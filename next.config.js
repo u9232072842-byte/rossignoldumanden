a59d8313f2eb1e8fefcf80248cd0520c73d35859
ousmane yattus <u9232072842@gmail.com>
2025-10-20 19:53:17 +0000

    toutes les photos dans le dossier public pas d'images unplash

--- next.config.js
+++ next.config.js
@@ -13,10 +13,6 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
     ],
   },
 };
