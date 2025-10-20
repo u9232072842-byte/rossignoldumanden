882968a740f6d46a2f6673bb34720a71a1183c6b
ousmane yattus <u9232072842@gmail.com>
2025-10-20 18:58:36 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
